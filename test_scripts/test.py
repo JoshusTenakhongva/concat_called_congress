d95e5c03f7ee86a48ce21f96d2e858c5d58b0be3
Joshus Tenakhongva <joshusttenakhongva@gmail.com>
2022-04-23 12:37:22 -0700

    all apis successfully draw legit json files

--- test_scripts/test.py
+++ test_scripts/test.py
@@ -2,30 +2,42 @@ import requests, json, os
 
 def main(): 
     # Initialization
+    # Set up requests, headers, and parameters
     propublica_key= os.getenv( 'propub_key' )
     propub_header= { 'X-API-Key':propublica_key }
-    propub_params= { 'congress': 117, 'chamber': 'senate' }
-    propub_base= 'https://api.propublica.org/congress/v1/117/senate'
+    propub_params= {}
+    propub_base= 'https://api.propublica.org/congress/v1/117/nominees/confirmed.json'
 
     regulations_key= os.getenv( 'reg_key' )
-    reg_headers= {'X-Api-Key': regulations_key, 'Content-Type': 'application/vnd.api+json' }
+    reg_headers= {'X-Api-Key':regulations_key, 'Content-Type':'application/vnd.api+json' }
     reg_params= {}
     reg_base= 'https://api.regulations.gov/v4/documents'
 
     openfec_key= os.getenv( 'fec_key' )
-    fec_params= {'api_key': openfec_key, 'q': 'erik aadland' }
+    fec_params= {'api_key':openfec_key, 'q':'erik aadland' }
     fec_base= 'https://api.open.fec.gov/v1'
     fec_query= '/candidates/search/'
 
+    # Send requests
     propublica_r= requests.get( propub_base, headers=propub_header, params=propub_params )
     regulations_r= requests.get( reg_base, headers=reg_headers ) 
     openfec_r= requests.get( fec_base+fec_query, params=fec_params )
 
+    # Check status codes
     print( 'proPublica: ' + str(propublica_r.status_code) )
     print( 'regulations: ' + str(regulations_r.status_code) )
     print( 'open fec: ' + str(openfec_r.status_code) )
 
-    # Save response
+    # Save response in Json format
+    write_json( 'propublica.json', propublica_r.json() )
+    write_json( 'regulations.json', regulations_r.json() )
+    write_json( 'openfec.json', openfec_r.json() )
+
+    # Close responses
+    propublica_r.close()
+    regulations_r.close()
+    openfec_r.close() 
+
 
 def write_json( filename, content ): 
 
